2886f668bcbcfee7a31f39faaf4251ab7848e4a0
Martin Zeitler <syslogic@users.noreply.github.com>
2017-05-12 07:04:34 +0200

    fixed for Android Emulator 26.0.3
    one could check further... for example, if the `emulator --version` matches.

--- wrap_android_emulator.sh
+++ wrap_android_emulator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# wraps the Android Emulator with CLI options
+# it wraps the Android Emulator with CLI options
 # written 2017-03-07 by Martin Zeitler
 
-# Size of Android QEMU 25.3.1.0 (build_id 3776547)
-ANDROID_EMU_MIN_SIZE=709824
+# Size of Android Emulator 26.0.3.0 (build_id 3965150)
+ANDROID_EMU_MIN_SIZE=625840
 
 ANDROID_QEMU_CLI_PARAMS="-qemu -vga cirrus -m 2G -k de -snapshot -enable-kvm"
 ANDROID_EMU_CLI_PARAMS="-use-system-libs -no-boot-anim -verbose -gpu swiftshader"
@@ -17,11 +17,11 @@ else
     echo "the Android QEMU emulator is already wrapped;"
     # checking if the emulator was already moved
     if [ $(stat -c%s "$ANDROID_EMU_FILE_NAME.bin") -ge $ANDROID_EMU_MIN_SIZE ]; then
-        echo "just updating the CLI parameters being passed."
+        echo "just updating the CLI paramters being passed."
     else
         # terminate execution and indicate an error 
 	exit 1 
-    fi   
+    fi
 fi
 
 # create a bash script
@@ -30,3 +30,4 @@ echo "set -ex" >> "$ANDROID_EMU_FILE_NAME"
 echo "\$0.bin \$@ $ANDROID_EMU_CLI_PARAMS $ANDROID_QEMU_CLI_PARAMS" >> "$ANDROID_EMU_FILE_NAME"
 ANDROID_EMU_SCRIPT_SIZE=$(stat -c%s "$ANDROID_EMU_FILE_NAME")
 echo "the patch has been applied, $ANDROID_EMU_SCRIPT_SIZE were written."
+
