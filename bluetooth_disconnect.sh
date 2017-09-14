3c91608dfd2e8f03a2ef6df5df67664955773163
Martin Zeitler <syslogic23@gmail.com>
2017-09-14 14:39:15 +0200

    comments added.

--- bluetooth_disconnect.sh
+++ bluetooth_disconnect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-command -v bluetoothctl >/dev/null 2>&1 || { echo >&2 "bluetoothctl is not installed.."; exit 1; }
+# $1 is a Bluetooth MAC address.
+# example usage: bluetooth_connect.sh 00:00:00:00:00:00
+# currently being triggered by /usr/lib/udev/rules.d/60-soundcore.rules
+command -v bluetoothctl >/dev/null 2>&1 || { echo >&2 "bluetoothctl is not installed."; exit 1; }
 
 MAC="00:00:00:00:00:00"
 if [ "$#" -ge  "1" ] then
