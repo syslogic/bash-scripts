47022f9015286ecab56bba78b96a41386912b156
Martin Zeitler <syslogic23@gmail.com>
2017-09-14 14:23:18 +0200

    check for $1 added (so udev can pass the MAC address).

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 MAC="00:00:00:00:00:00"
-echo "connecting the SoundCore @ $MAC!"
+if [ "$#" -ge  "1" ] then
+  MAC=$1
+fi
+echo "connecting Bluetooth Device @ $MAC!"
 echo -e "trust $MAC\nconnect $MAC\nquit" | bluetoothctl
 # sleep 20
--- bluetooth_disconnect.sh
+++ bluetooth_disconnect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 MAC="00:00:00:00:00:00"
-echo "disconnecting the SoundCore @ $MAC!"
+if [ "$#" -ge  "1" ] then
+  MAC=$1
+fi
+echo "disconnecting Bluetooth Device @ $MAC!"
 echo -e "disconnect $MAC\nquit" | bluetoothctl
 # sleep 20
