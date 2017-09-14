acdaca4e164d84e8665057f0f952affa7331423a
Martin Zeitler <syslogic23@gmail.com>
2017-09-14 14:32:13 +0200

    checking for "command -v bluetoothctl".

--- bluetooth_disconnect.sh
+++ bluetooth_disconnect.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+command -v bluetoothctl >/dev/null 2>&1 || { echo >&2 "bluetoothctl is not installed.."; exit 1; }
+
 MAC="00:00:00:00:00:00"
 if [ "$#" -ge  "1" ] then
   MAC=$1
 fi
+
 echo "disconnecting Bluetooth Device @ $MAC!"
 echo -e "disconnect $MAC\nquit" | bluetoothctl
 # sleep 20
