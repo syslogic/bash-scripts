011515ac7f2cb2550248d9f5bfe9bbaa7f4f0f75
Martin Zeitler <syslogic23@gmail.com>
2017-09-14 14:49:55 +0200

    tested & syntax fixed.

--- bluetooth_disconnect.sh
+++ bluetooth_disconnect.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # $1 is a Bluetooth MAC address.
-# example usage: bluetooth_disconnect.sh 00:00:00:00:00:00
+# example usage: bluetooth_disconnect.sh 01:02:03:04:05:06
 # currently being triggered by /usr/lib/udev/rules.d/60-soundcore.rules
 command -v bluetoothctl >/dev/null 2>&1 || { echo >&2 "bluetoothctl is not installed."; exit 1; }
 
-MAC="00:00:00:00:00:00"
-if [ "$#" -ge  "1" ] then
+MAC="08:EB:ED:F6:53:A3"
+if [ "$#" -ge  "1" ]
+then
   MAC=$1
 fi
 
 echo "disconnecting Bluetooth Device @ $MAC!"
 echo -e "disconnect $MAC\nquit" | bluetoothctl
-# sleep 20
+# sleep 10
